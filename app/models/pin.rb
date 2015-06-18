cbc11b087113493931ec24279aca81b82ed31cf1
tolehico <tolehico@gmail.com>
2015-06-18 23:56:30 +0900

    Add migration file

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,4 @@
 class Pin < ActiveRecord::Base
-	attr_accessor :image_file_name
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	# allows a pin to have an attached file image
